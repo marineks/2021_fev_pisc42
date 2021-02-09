174ab8b26513d8cf742416217d6100675507780a
Marine SANJUAN <msanjuan@e3r4p14.42.fr>
2021-02-09 09:44:29 +0100

    retry C 00

---
+++ retry_C_01/ex05/ft_print_comb.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/05 08:06:42 by msanjuan          #+#    #+#             */
+/*   Updated: 2021/02/09 09:42:55 by msanjuan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_res(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a != '7')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_res(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
