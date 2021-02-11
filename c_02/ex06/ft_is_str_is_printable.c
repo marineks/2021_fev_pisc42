596badab0e847e25a3ebff1d48dba361e5cfb1fd
Marine SANJUAN <msanjuan@e3r4p16.42.fr>
2021-02-11 12:59:35 +0100

    exo6 done - ft is str printable

---
+++ c_02/ex06/ft_is_str_is_printable.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_str_is_printable.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 12:56:02 by msanjuan          #+#    #+#             */
+/*   Updated: 2021/02/11 12:56:04 by msanjuan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int     ft_str_is_printable(char *str)
+{
+    int counter;
+    int res_final;
+    char is_str_printable;
+    
+    counter = 0;
+    res_final = 1;
+    while (str[counter] != '\0')
+    {
+        is_str_printable = str[counter] >= 32 && str[counter] <= 126 ; // en table ascii (https://learn.sparkfun.com/tutorials/ascii/all)
+        if (!is_str_printable) 
+        {
+            res_final = 0;
+        } 
+        counter ++;
+    }
+    printf("%d", res_final);
+    return (res_final);
+}
+
+int     main(void)
+{
+    char test[] = {'a', 'b', 'c'};
+
+    ft_str_is_printable(test);
+    return (0);
+}
