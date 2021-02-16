8d598177d9adc46701a1bb7a06a34e4c297325fd
Marine Sanjuan <marine@sanjuan.eu>
2021-02-16 22:19:51 +0100

    recherche atoi

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -3,16 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 15:50:01 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/02/16 15:56:18 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/02/16 22:16:04 by marine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_atoi(char *str)
 {
 
+    // definir les white spaces (i++ tant qu'on a ce genre de caract)
+
+    // gérer les signes negatifs et positifs (si nombre impair de -,
+    // signe devient negatif - à gérer avec le modulo)
+
+    // dire que si cest pas compris entre 0 et 9, arrêter de boucler
+
+    // convertir le resultat char to int avec *10 into table ascii ( - 48?)
+
+    // multiplier le resultat par le signe obtenu
 }
 
 /*
