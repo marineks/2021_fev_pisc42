0f30979933de13a4bdbc3ea729d2d5c8980327d6
Marine SANJUAN <msanjuan@e3r7p16.42.fr>
2021-02-16 15:58:32 +0100

    debut atoi

---
+++ c_04/ex03/ft_atoi.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/16 15:50:01 by msanjuan          #+#    #+#             */
+/*   Updated: 2021/02/16 15:56:18 by msanjuan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_atoi(char *str)
+{
+
+}
+
+/*
+** str peut commencer par un nombre arbitraire de ‘white space‘
+**  The isspace() and isspace_l() functions test for whitespace characters.
+**  In the C locale, the complete list of whitespace characters is:
+**    ‘ ’ Space character.
+**    \f  Form feed.
+**    \n  New-line.
+**    \r  Carriage return.
+**    \t  Horizontal tab.
+**    \v  And vertical tab.
+*/
