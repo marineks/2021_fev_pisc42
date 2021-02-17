c7e3128c366ab3d0b79494962bb8ee03e56c510f
Marine SANJUAN <msanjuan@e3r1p6.42.fr>
2021-02-17 10:27:34 +0100

    ATOIIIIII LES OPTICIENS

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -3,36 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 15:50:01 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/02/16 22:16:04 by marine           ###   ########.fr       */
+/*   Updated: 2021/02/17 10:27:07 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int ft_atoi(char *str)
 {
+    int i;
+    int res_value;
+    int sign_of_value;
+    int neg_count;
+    int pos_count;
+    
+    i = 0;
+    res_value = 0;
+    sign_of_value = 1;
+    neg_count = 0;
+    pos_count = 0;
 
     // definir les white spaces (i++ tant qu'on a ce genre de caract)
+    while ( str[i] && (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t' || str[i] == '\v'))
+        i++;
+    // gérer les signes negatifs et positifs (si nombre impair de -, signe devient negatif - à gérer avec le modulo?)
+    while (str[i] && (str[i] == '+' || str[i] == '-'))
+    {
+        str[i] == '-' ? neg_count++ : pos_count++;
+        i++;
+    }
+    (neg_count > pos_count && (neg_count % 2 != 0)) ? sign_of_value = -1 : sign_of_value;
+    while (str[i] && (str[i] > 47 && str[i] < 58)) // dire que si cest pas compris entre 0 et 9, arrêter de boucler
+    {
+        res_value = res_value * 10 + str[i] - 48; // convertir le resultat char to int
+        i++;
+    }
+    res_value *= sign_of_value; // multiplier le resultat par le signe obtenu
 
-    // gérer les signes negatifs et positifs (si nombre impair de -,
-    // signe devient negatif - à gérer avec le modulo)
-
-    // dire que si cest pas compris entre 0 et 9, arrêter de boucler
-
-    // convertir le resultat char to int avec *10 into table ascii ( - 48?)
-
-    // multiplier le resultat par le signe obtenu
+    return (res_value);
 }
 
-/*
-** str peut commencer par un nombre arbitraire de ‘white space‘
-**  The isspace() and isspace_l() functions test for whitespace characters.
-**  In the C locale, the complete list of whitespace characters is:
-**    ‘ ’ Space character.
-**    \f  Form feed.
-**    \n  New-line.
-**    \r  Carriage return.
-**    \t  Horizontal tab.
-**    \v  And vertical tab.
-*/
+int main()
+{
+    char test[] = " ---+--+1234ab567";
+    printf("%d", ft_atoi(test));
+    return (0);
+}
