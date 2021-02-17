2894437134099ca2a4c4e4a21e319ef0057b6261
Marine Sanjuan <marine@sanjuan.eu>
2021-02-17 19:07:31 +0100

    exo1 done

--- c_06/ex01/ft_print_params.c
+++ c_06/ex01/ft_print_params.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 15:14:19 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/02/17 15:34:19 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/02/17 19:05:01 by marine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int     ft_strlen(char *argv)
+int ft_strlen(char *argv)
 {
     int i;
 
@@ -23,24 +23,23 @@ int     ft_strlen(char *argv)
     }
     return (i);
 }
-void    ft_print(char *argv)
+void ft_print(char *argv)
 {
-        write(1, argv, ft_strlen(argv));
+    write(1, argv, ft_strlen(argv));
 }
 
 int main(int argc, char **argv)
 {
     int i;
 
-    i = 0;
+    i = 1;
     if (argc > 0)
     {
-        while (argv[i] != 0)
+        while (i < argc && argv[i] != 0)
         {
-            ft_print(argv[i + 1]);
+            ft_print(argv[i]);
             write(1, "\n", 1);
             i++;
         }
-        
     }
 }
